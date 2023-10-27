f02cb89fb8aecca9b1b5056a272b7a7b3c38c13f
ROGUEANOVI <ovidioromero66@gmail.com>
2023-10-27 08:59:32 -0500

    Initial commit

---
+++ src/main/java/com/rogueanovi/layeredarchitectureapi/LayeredArchitectureApiApplication.java
@@ -0,0 +1,13 @@
+package com.rogueanovi.layeredarchitectureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredArchitectureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayeredArchitectureApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogueanovi/layeredarchitectureapi/LayeredArchitectureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogueanovi.layeredarchitectureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayeredArchitectureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
